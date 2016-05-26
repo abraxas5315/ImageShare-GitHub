4b3ee2029a15197f18779b086585d2763478de9c
embex-user <embex-user@PC35301001>
2016-05-26 10:53:04 +0900

    selectMyPosts作成

--- ImageShare-GitHub/src/db/PostsDAO.java
+++ ImageShare-GitHub/src/db/PostsDAO.java
@@ -32,12 +32,24 @@ public class PostsDAO{
 			stmt.executeUpdate();
 				}
 	}
+
+	/**
+	 * データベースからＴＬを取得する
+	 * @param member ログインしている会員情報
+	 * @return 取得したＴＬ
+	 * @throws SQLException
+	 * @author s.kawashima
+	 */
 	public List<Article> selectTL(Member member) throws SQLException
 	{
-		List<Member> follows = null;
+		//フォローリスト
+		List<Member> follows = new ArrayList<Member>();
+		//TL格納用のリスト
 		List<Article> articles = new ArrayList<Article>();
+
+		//フォローリスト取得用のSQL文
 		StringBuilder str = new StringBuilder();
-		str.append("SELECT t2.account_id,t1.name,t1.my_image,t1.profile ");
+		str.append("SELECT t2.account_id , name , my_image , profile ");
 		str.append("FROM m_account t1,t_follow t2 ");
 		str.append("WHERE t1.account_id = t2.account_id  ");
 		str.append("AND t2.follow_id= ? ");
@@ -50,7 +62,6 @@ public class PostsDAO{
 			pstmt.setString(1, member.getAccountId());
 			ResultSet res = pstmt.executeQuery();
 
-			follows = new ArrayList<Member>();
 			while (res.next()) {
 				follows.add(new Member(
 						(res.getString("account_id")),
@@ -60,34 +71,36 @@ public class PostsDAO{
 						));
 			}
 
-			for(int i=0 ; i<follows.size() ; i++)
-			{
-				System.out.println(follows.get(i).getAccountId());
-			}
+			//TL取得用のSQL文
+			query = "SELECT account_id , image_url , text , date FROM t_article WHERE account_id IN(?)";
 
-			query = "SELECT * FROM t_article WHERE account_id=IN(?)";
-			StringBuilder str2 = new StringBuilder();
-			str2.append(member.getAccountId());
-			for(Member f : follows)
+			try (
+					PreparedStatement pstmt2 = con.prepareStatement(query);)
 			{
-				str.append(",").append(f.getAccountId());
-			}
-			pstmt.setString(1, str.toString());
-			try(
-				PreparedStatement pstmt2 = con.prepareStatement(query);)
+				StringBuilder follow = new StringBuilder();
+				//IN句に追加する条件の作成（フォローとログインしているユーザのaccountId）
+				follow.append(member.getAccountId());
+				for(Member f : follows)
 				{
+					follow.append(",").append(f.getAccountId());
+				}
+				// コードを設定
+				pstmt.setString(1, follow.toString());
 
-				ResultSet res2 = pstmt.executeQuery();
+				ResultSet res2 = pstmt2.executeQuery();
 				Member fMember = null;
 
+				//取得したＴＬの格納
 				while (res2.next()) {
 					for(Member f : follows){
+						//記事が誰のものと一致しているかをチェック
 						if(res2.getString("") != f.getAccountId())
 						{
 							fMember = f;
 							break;
 						}
 					}
+					//記事の格納
 					articles.add(new Article(
 							fMember,
 							(res2.getString("image_url")),
@@ -97,11 +110,44 @@ public class PostsDAO{
 				}
 			}
 		}
-		for(int i=0 ; i<articles.size() ; i++)
-		{
-			System.out.println(articles.get(i).getAccountId());
-		}
+		return articles;
+	}
 
+
+	/**
+	 * 個人の投稿を取得する
+	 * @param member 表示する個人ページの会員情報
+	 * @return 取得した個人の投稿
+	 * @throws SQLException
+	 */
+	public List<Article> selectMyPosts(Member member) throws SQLException
+	{
+		//投稿格納用のリスト
+		List<Article> articles = new ArrayList<Article>();
+
+		//個人投稿取得
+		String query = "SELECT image_url , text , date FROM t_article WHERE account_id = ?";
+
+		DataSourceSupplier supplier = DataSourceSupplier.getInstance();
+		try (Connection con = supplier.getConnection();
+				PreparedStatement pstmt = con.prepareStatement(query);)
+		{
+			//コード設定
+			pstmt.setString(1, member.getAccountId());
+
+			ResultSet res2 = pstmt.executeQuery();
+
+			//取得した1個人投稿の格納
+			while (res2.next()) {
+				//記事の格納
+				articles.add(new Article(
+						member,
+						(res2.getString("image_url")),
+						(res2.getString("text")),
+						(res2.getDate("date"))
+						));
+			}
+		}
 		return articles;
 
 	}
